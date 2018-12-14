13eea46c6fbbccbc6636f088fd33d90afb6018ee
it-toppp <36742579+it-toppp@users.noreply.github.com>
2018-12-14 23:15:38 +0300

    Update anbox.sh

--- anbox.sh
+++ anbox.sh
@@ -1,8 +1,14 @@
 sudo add-apt-repository ppa:morphis/anbox-support
 sudo apt update
-sudo apt install anbox-modules-dkms snap -y
-
+sudo apt install anbox-modules-dkms snap wget lzip unzip squashfs-tools -y
 sudo modprobe ashmem_linux
 sudo modprobe binder_linux
 
 sudo snap install --devmode --beta anbox
+sudo apt-get install android-sdk-platform-tools
+
+wget https://www.cdn.whatsapp.net/android/2.18.379/WhatsApp.apk
+adb install WhatsApp.apk
+
+wget https://download.teamviewer.com/download/linux/teamviewer_amd64.deb
+apt install ./teamviewer_amd64.deb -y
