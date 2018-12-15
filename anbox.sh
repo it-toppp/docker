8519bd3dd166b864d5d8521b0f89ba3ed2aede9b
it-toppp <36742579+it-toppp@users.noreply.github.com>
2018-12-15 11:20:39 +0300

    Update anbox.sh

--- anbox.sh
+++ anbox.sh
@@ -1,15 +1,13 @@
-sudo add-apt-repository ppa:morphis/anbox-support
+sudo add-apt-repository ppa:morphis/anbox-support -y
 sudo apt update
 sudo apt install anbox-modules-dkms snap wget lzip unzip squashfs-tools -y
 sudo modprobe ashmem_linux
 sudo modprobe binder_linux
 
 sudo snap install --devmode --beta anbox
-sudo apt-get install android-sdk-platform-tools
+#sudo apt-get install android-sdk-platform-tools
 
-
-sudo apt install android-tools-adb
-
+sudo apt install android-tools-adb -y
 
 wget https://www.cdn.whatsapp.net/android/2.18.379/WhatsApp.apk
 adb install WhatsApp.apk
