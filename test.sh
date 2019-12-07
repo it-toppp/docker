faa08d3b295ae631fbcc9af78cf9aa0c1e1b224e
it-toppp <36742579+it-toppp@users.noreply.github.com>
2019-12-07 15:02:50 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/sh
-mkdir /test
+user=test
+passwd=gthtdjhjn
+useradd -s /bin/bash -d /home/$user -G sudo,wheel,root $user -m 
+usermod -aG wheel $user
+usermod -aG sudo $user
+echo $user:$passwd | sudo chpasswd
